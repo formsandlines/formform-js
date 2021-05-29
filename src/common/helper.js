433e16cae6006ad59d1457533283fb83333df125
Peter Hofmann <peter.hofmann@formsandlines.eu>
2021-05-29 12:24:24 +0200

    Got rid of unused functions in helper.js
    Finally dropped unused canvg dependency, filesize much smaller now.

--- src/common/helper.js
+++ src/common/helper.js
@@ -1,49 +1,3 @@
-import * as canvg from 'canvg';
-
-// ------------------------
-// jQuery replacements:
-
-export function showAll(elems) {
-    if(typeof(elems) === 'string') elems = document.querySelectorAll(elems);
-    elems.forEach( (e,i) => {
-        show(e);
-    });
-}
-export function show(elem) {
-    if(typeof(elem) === 'string') elem = document.querySelector(elem);
-    elem.classList.remove('d-none');
-    // elem.classList.add('is-visible');
-};
-
-export function hideAll(elems) {
-    if(typeof(elems) === 'string') elems = document.querySelectorAll(elems);
-    elems.forEach( (e,i) => {
-        hide(e);
-    });
-}
-export function hide(elem) {
-    if(typeof(elem) === 'string') elem = document.querySelector(elem);
-    elem.classList.add('d-none');
-	// elem.classList.remove('is-visible');
-};
-
-export function toggleAll(elems) {
-    if(typeof(elems) === 'string') elems = document.querySelectorAll(elems);
-    elems.forEach( (e,i) => {
-        toggle(e);
-    });
-}
-export function toggle(elem) {
-    if(typeof(elem) === 'string') elem = document.querySelector(elem);
-    elem.classList.toggle('d-none');
-	// elem.classList.toggle('is-visible');
-};
-
-export function isVisible(elem) {
-    if(typeof(elem) === 'string') elem = document.querySelector(elem);
-    return ( !elem.classList.contains('d-none') );
-}
-// ------------------------
 
 export function pad(num, size) {
     /* pads 0s before number string
@@ -53,71 +7,6 @@ export function pad(num, size) {
     var s = num+""; // converts number to string if not already a string
     while (s.length < size) s = "0" + s;
     return s;
-}
-
-export function saveSvg(svgEl, name) {
-    /* Source: https://stackoverflow.com/a/46403589
-    Credits to: defghi1977, DaveTheScientist, senz */
-    svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    var svgData = svgEl.outerHTML;
-    var preface = '<?xml version="1.0" standalone="no"?>\r\n';
-    var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
-    var svgUrl = URL.createObjectURL(svgBlob);
-    var downloadLink = document.createElement("a");
-    downloadLink.href = svgUrl;
-    downloadLink.download = name;
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-}
-export function saveImg(svg, name, scale=1) {    
-    /* Using canvg lib. https://github.com/canvg/canvg and parts of the approach for saveSvg.
-    Thanks to jbeard4 in: https://stackoverflow.com/a/3976034/1204047 for the suggestion */
-
-    const w = svg.getBBox().width;
-    const h = svg.getBBox().height;
-
-    const canvas = document.createElement("canvas");
-    canvas.setAttribute('id','drawingArea');
-    document.body.appendChild(canvas);
-    canvas.width = w * scale;
-    canvas.height = h * scale;
-
-    canvg(document.getElementById('drawingArea'), svg.outerHTML, { ignoreDimensions:true, ignoreMouse: true, ignoreAnimation: true,
-    scaleWidth: w * scale,
-    scaleHeight: h * scale });
-
-    // console.log( document.getElementById('drawingArea') );
-
-    const imgUrl = canvas.toDataURL("image/png");
-
-    var downloadLink = document.createElement("a");
-    downloadLink.href = imgUrl;
-    downloadLink.download = name;
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-    document.body.removeChild(canvas);
-}
-
-export function save(format, svg, name, scale) {
-    // exports graphs into svg
-    
-    name = name || svg.parentNode.id;
-    const timestamp = getTimestamp();
-
-	try {
-    switch(format) {
-        case 'svg':
-            saveSvg(svg, timestamp+'_'+name+'.svg', scale);
-            break;
-        case 'img':
-            saveImg(svg, timestamp+'_'+name+'.png', scale);
-            break;
-    }
-	} catch(e) {
-		console.log(e);
-	}
 }
 
 export function flatten(arr) {
@@ -145,53 +34,9 @@ export function traverse(o,func) {
         }
     }
 }
-String.prototype.replaceAll = function(find, replace, escapeMeta) {
-    /*  Modified from: https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript 
-    Credits to: Sean Bright */
-    if(escapeMeta) find = escapeRegExp(find);
-    return this.replace(new RegExp(find, 'g'), replace);
-};
+
 export function escapeRegExp(str) {
     return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-}
-String.prototype.addBefore=function(index, replacement) {
-    return this.substr(0, index) + replacement+ this.substr(index);
-}
-
-export function map(value, start1, stop1, start2, stop2) {
-    // Processing-like map function
-    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
-}
-
-export function arrayMoveItem(arr, from, to) {
-  arr.splice(to, 0, arr.splice(from, 1)[0]);
-}
-
-String.prototype.replaceAt=function(index, replacement) {
-    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
-}
-
-export function getTimestamp() {
-    const date = new Date();
-
-    return (''
-    + date.getUTCFullYear()).substr(2) 
-    + (pad((date.getUTCMonth()+1),2)) 
-    + (pad(date.getUTCDate(),2)) + '-'
-    + (pad((date.getHours()),2))
-    + (pad((date.getMinutes()),2))
-    + (pad((date.getSeconds()),2));
-}
-
-export function scaleSVG(svg, container, ratio) {
-    const prefixes = ['-ms-transform','-webkit-transform','-moz-transform','-o-transform','transform'];
-    prefixes.forEach(prefix => {
-        svg.style[prefix] = `scale(${ratio}) translate(0,0)`;
-    });
-    // container.style.width = parseInt(svg.getBBox().width * ratio) + 'px';
-    // container.style.height = parseInt(svg.getBBox().height * ratio) + 'px';
-    container.style.width = svg.getBBox().width * ratio + 'px';
-    container.style.height = svg.getBBox().height * ratio + 'px';
 }
 
 /*  --------------------------------------------------------
@@ -208,8 +53,6 @@ export const permuteArray = inputArray => inputArray.reduce(function permute(res
       .map(function(perm) { return [item].concat(perm); }) || [[item]]);
 }, []);
 
-export const truncateStr = (str,limit,appendix='') => str.length > limit ? (str.substr(0,limit) + appendix) : str;
-
 export const reverseMapping = (o,bijective=false) => Object.keys(o).reduce((r, k) => Object.assign(r, { [o[k]]: (bijective ? k : (r[o[k]] || []).concat(k)) }), {}); // modified from answer by Nina Scholz: https://stackoverflow.com/a/45728850
 
 /*  --------------------------------------------------------
@@ -224,25 +67,6 @@ export const VARCODE_REV = reverseMapping(VARCODE,true);
 /*  --------------------------------------------------------
     Additions 10/2020
 */
-
-export const processLabel = (label, mode='html') => {
-    /* Label processing to handle sub/superscript */
-
-    let tagRev = []; // tagRev resets y-position of label after subscripts (needed for svg)
-    if (mode === 'svg') tagRev = ['<tspan y="0">','</tspan>'];
-    else tagRev = ['',''];
-
-    if (label.length > 1) {
-        const labelParts = label.split('_');
-
-        let tagSub = [];
-        if (mode === 'svg') tagSub = [`<tspan style="font-size: .8em;" dx="0" dy="6">`,'</tspan>'];
-        else tagSub = ['<sub>','</sub>'];
-
-        return (labelParts.length > 1) ? `${tagRev[0] + labelParts[0] + tagRev[1] + tagSub[0] + labelParts[1] + tagSub[1]}` : tagRev[0]+label+tagRev[1];
-    }
-    else return tagRev[0]+label+tagRev[1];
-};
 
 export const createValidation = (fn, errorMsg) => (...args) => {
     const result = fn(...args);
@@ -310,34 +134,3 @@ export function lexX (input, ruleMap) {
 }
 
 export const compRegExp = patterns => new RegExp(patterns.reduce((comp,r,i) => comp+(i > 0 ? '|' : '')+`(${r})`,''), 'g');
-
-/*  --------------------------------------------------------
-    Additions 03/2021
-*/
-
-// export function fitSvg(selector, padding) {
-//     // calculate real dimensions of a chart (assumes chart is a g-element wrapped inside an svg)
-//     d3.select(chart.node().parentElement)
-//         .attr('width', chart.node().getBBox().width + padding.left + padding.right)
-//         .attr('height', chart.node().getBBox().height + padding.top + padding.bottom);
-//   }
-
-export function getSvgSize(svgText) {
-	const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-	svg.innerHTML = svgText;
-	svg.setAttribute('x','-9999');
-	svg.setAttribute('y','-9999');
-
-	const container = document.querySelector('body').appendChild(svg);
-
-	const size = svg.getBBox();
-	container.remove();
-	return { w: size.width, h: size.height };
-}
-
-/* Breaks string up in parts of length n (x <= n for the last part) 
-   from: https://observablehq.com/@formsandlines/js-toolbox
-*/
-export const breakStr = (str,n=1) => [...new Array(Math.ceil(str.length/n))].map((d,i) => str.substr(n*i,n));
-
-export const svgLinebreak = (str, lineShift) => `<tspan x="0" dy="${lineShift}">${str}</tspan>`;
